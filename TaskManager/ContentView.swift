6fbe8771ac367b7487ad6786107bc4c25e4dfce1
William Harrington <wharrington@lcs.on.ca>
2021-11-24 11:33:03 -0500

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by William Robert Harrington on 2021-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by William Robert Harrington on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
